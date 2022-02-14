2b07679fe787343f710ca2f7bd2b6745701f74fc
parappa20 <88298540+james182-dang@users.noreply.github.com>
2022-02-14 12:45:41 -0600

    update Contact section styling

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -39,42 +39,55 @@ function Contact() {
     }
 
     return (
-        <div className='contact-container d-flex justify-content-center row'>
-            <h1>Contact Me</h1>
-            <section className='contact col-lg-4 col-md-8 col-sm-8'>
-                <div>
-                <form id="contact-form" onSubmit={handleSubmit}>
-                    <div className='form-box'>
-                        <label htmlFor='name'>Name:</label>
-                        <input type='text' defaultValue={name} onBlur={handleChange} name='name' />
-                    </div>
-                    <div className='form-box'>
-                        <label htmlFor='email'>Email address:</label>
-                        <input type='email' defaultValue={email} onBlur={handleChange} name='email' />
-                    </div>
-                    <div className='form-box'>
-                        <label htmlFor='message'>Message:</label>
-                        <textarea name='message' defaultValue={message} onBlur={handleChange} rows='4' />
-                    </div>
-
-                    {errorMessage && (
-                        <div>
-                            <p className='error-text'>{errorMessage}</p>
-                        </div>
-                    )}
-
-                <button type='submit'>Submit</button>
-            </form>
+        <div className='contact-container'>
+            <div className='contactContent'>
+                <div class="background">
+                    <div class="shape"></div>
+                    <div class="shape"></div>
+                </div>
+                <form>
+                    <input name="name" type="text" class="feedback-input" placeholder="Name" />
+                    <input name="email" type="text" class="feedback-input" placeholder="Email" />
+                    <textarea name="text" class="feedback-input" placeholder="Comment"></textarea>
+                    <input type="submit" value="Submit" />
+                </form>
             </div>
-
-            </section>
-
-            <section className='info col-lg-4 col-md-8 col-sm-8'>
-                <h3>Want to contact me? Fill in the boxes and I will reach out to you as soon as possible!</h3>
-            </section>
         </div>
 
     );
 }
 
 export default Contact;
+
+                {/* <h1>Contact Me</h1>
+                <section className='contact col-lg-4 col-md-8 col-sm-8'>
+                    <div>
+                        <form id="contact-form" onSubmit={handleSubmit}>
+                            <div className='form-box'>
+                                <label htmlFor='name'>Name:</label>
+                                <input type='text' defaultValue={name} onBlur={handleChange} name='name' />
+                            </div>
+                            <div className='form-box'>
+                                <label htmlFor='email'>Email address:</label>
+                                <input type='email' defaultValue={email} onBlur={handleChange} name='email' />
+                            </div>
+                            <div className='form-box'>
+                                <label htmlFor='message'>Message:</label>
+                                <textarea name='message' defaultValue={message} onBlur={handleChange} rows='4' />
+                            </div>
+
+                            {errorMessage && (
+                                <div>
+                                    <p className='error-text'>{errorMessage}</p>
+                                </div>
+                            )}
+
+                            <button type='submit'>Submit</button>
+                        </form>
+                    </div>
+
+                </section>
+
+                <section className='info col-lg-4 col-md-8 col-sm-8'>
+                    <h3>Want to contact me? Fill in the boxes and I will reach out to you as soon as possible!</h3>
+                </section> */}
